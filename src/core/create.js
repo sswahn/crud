378213ee4d9468fc34f484548f2e3d1cfe75a7ee
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-12 10:13:54 -0500

    Update create.js

--- src/core/create.js
+++ src/core/create.js
@@ -1,33 +1,37 @@
 
-const setType = (value) => {
+const setType = value => {
   if (typeof value === "string") {
-    return { S: value };
-  } else if (typeof value === "number") {
-    return { N: value.toString() };
-  } else if (typeof value === "boolean") {
-    return { BOOL: value };
-  } else if (Buffer.isBuffer(value)) {
-    return { B: value.toString("base64") };
-  } else if (value === null || value === undefined) {
-    return { NULL: true };
-  } else if (Array.isArray(value)) {
-    return { L: value.map((item) => getDynamoDBType(item)) };
-  } else if (typeof value === "object") {
+    return { S: value }
+  }
+  if (typeof value === "number") {
+    return { N: value.toString() }
+  }
+  if (typeof value === "boolean") {
+    return { BOOL: value }
+  } 
+  if (Buffer.isBuffer(value)) {
+    return { B: value.toString("base64") }
+  } 
+  if (value === null || value === undefined) {
+    return { NULL: true }
+  }
+  if (Array.isArray(value)) {
+    return { L: value.map((item) => getDynamoDBType(item)) }
+  }
+  if (typeof value === "object") {
     return { M: Object.entries(value).reduce((acc, [key, value]) => ({ ...acc, [key]: setType(value) }), {}) }
   }
-  // Handle other types as needed
-  return { NULL: true }
 }
 
 const create = params => {
   if (!process.env.TABLE_NAME) {
-    throw new ReferenceError('create: TABLE_NAME environmental variable is required.')
+    throw new ReferenceError('TABLE_NAME environmental variable is required.')
   }
   if (typeof process.env.TABLE_NAME !== 'string') {
-    throw new TypeError('create: TABLE_NAME must be of type string.')
+    throw new TypeError('TABLE_NAME must be of type string.')
   }
   if (typeof params !== 'object') {
-    throw new TypeError('create: argument must be of type object.')
+    throw new TypeError('Argument must be of type object.')
   }
   try {
     const items = Object.entries(params).reduce((acc, [key, value]) => ({ ...acc, [key]: setType(value) }), {})
@@ -40,6 +44,6 @@ const create = params => {
     const command = new PutItemCommand(values)
     return client.send(command)
   } catch (error) {
-    throw new Error(error)
+    throw new Error(`create: ${error}`)
   }
 }
